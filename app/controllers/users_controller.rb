fa1975214338887b96fa51ab86db7ca974e62ed3
ruby.jiro <cddpt903@yahoo.co.jp>
2018-11-24 05:42:45 +0000

    update user submit

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,4 @@
 class UsersController < ApplicationController
-  
   before_action :require_user_logged_in, only: [:index, :show]
   
   def index
@@ -25,19 +24,12 @@ class UsersController < ApplicationController
       flash.now[:danger] = 'ユーザの登録に失敗しました。'
       render :new
     end
+   end
   
   private
 
   def user_params
     params.require(:user).permit(:name, :email, :password, :password_confirmation)
   end
-  
-  end
-  
-  
-  
-  
-  
-  
   
 end
